96410035e8e1f1a71dd8247b0b4155af34f8b477
adgs <dummy>
2022-03-15 21:15:44 +0200

    Added  start function go routine loop for disk collector

--- diskstats/DiscUsageDataExtractor.go
+++ diskstats/DiscUsageDataExtractor.go
@@ -2,14 +2,25 @@ package diskstats
 
 import (
 	"io/ioutil"
+	"time"
 
 	"github.com/adgs85/gomonagent/agentlogger"
 	"github.com/adgs85/gomonagent/agentmessagesdispatcher"
+	"github.com/adgs85/gomonmarshalling/monmarshalling"
 
 	"github.com/ricochet2200/go-disk-usage/du"
 )
 
-func CollectDiskInfo(path string, sink agentmessagesdispatcher.StatSinkFuncType) {
+func StartDiskInfoLoopGoRoutine(sink agentmessagesdispatcher.StatSinkFuncType) {
+	go func() {
+		for {
+			sink(collectDiskInfo(cfg.DiskFreeSpacePath, sink))
+			time.Sleep(time.Duration(cfg.DiskPollingRateMs) * time.Millisecond)
+		}
+	}()
+}
+
+func collectDiskInfo(path string, sink agentmessagesdispatcher.StatSinkFuncType) monmarshalling.Stat {
 	fileInfo, err := ioutil.ReadDir(path)
 	if err != nil {
 		agentlogger.Logger().Fatalln(err)
@@ -21,5 +32,5 @@ func CollectDiskInfo(path string, sink agentmessagesdispatcher.StatSinkFuncType)
 		arr = append(arr, NewDiskStateEntry(dUsage.Size(), dUsage.Available(), fullPath))
 	}
 
-	sink(CreatePayload(NewDiskStatMetadata(cfg), arr))
+	return CreatePayload(NewDiskStatMetadata(cfg), arr)
 }
